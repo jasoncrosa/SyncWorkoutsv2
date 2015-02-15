e75ae4cbdda1e45a5ee4afd448bca74c46185019
Alain Defrance <defrancea@gmail.com>
2015-02-15 15:30:22 -0800

    Fix deployment

--- webapp/src/main/java/syncworkout/endpoint/HelloWorld.java
+++ webapp/src/main/java/syncworkout/endpoint/HelloWorld.java
@@ -1,4 +1,4 @@
-package syncworkout;
+package syncworkout.endpoint;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -7,11 +7,12 @@ import javax.ws.rs.Produces;
 /**
  * Created by Jason on 15/02/2015.
  */
-// The Java class will be hosted at the URI path "/helloworld"
-@Path("/helloworld")
+// The Java class will be hosted at the URI path "/rest/hello/world"
+@Path("hello")
 public class HelloWorld {
     // The Java method will process HTTP GET requests
     @GET
+    @Path("world")
     // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces("text/plain")
     public String getClichedMessage() {
