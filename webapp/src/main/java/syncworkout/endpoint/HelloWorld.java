fbf3ad500bbfe0a4219e0b676c5506e73451a8fa
Alain Defrance <defrancea@gmail.com>
2015-02-22 10:48:12 -0800

    Revert "swagger-core integration in progress..."
    This reverts commit 770fb4593136d63e83cd2beb47653ae067e3f482.

--- webapp/src/main/java/syncworkout/endpoint/HelloWorld.java
+++ webapp/src/main/java/syncworkout/endpoint/HelloWorld.java
@@ -1,10 +1,5 @@
 package syncworkout.endpoint;
 
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiResponses;
-import com.wordnik.swagger.annotations.ApiResponse;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -14,35 +9,14 @@ import javax.ws.rs.Produces;
  */
 // The Java class will be hosted at the URI path "/rest/hello/world"
 @Path("hello")
-@Api(value = "/hello", description = "Say Hello World !")
 public class HelloWorld {
     // The Java method will process HTTP GET requests
     @GET
     @Path("world")
     // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces("text/plain")
-    @ApiOperation(value="Hello World",
-            notes="Hello World with REST",
-            response=String.class,
-            responseContainer = "")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "OK"),
-            @ApiResponse(code = 500, message = "Something wrong in Server")})
     public String getClichedMessage() {
         // Return some cliched textual content
         return "Hello World";
     }
-
-    @GET
-    @Path("auth")
-    @Produces("text/plain")
-    public String auth() {
-        try {
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "Hello World";
-    }
-
 }
